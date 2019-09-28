2c16c26ef7e83870d78284580e7a7c60907511cd
Bryan Barker <bryan.barker@asu.edu>
2019-09-28 16:58:25 -0700

    adding navbar stories

--- stories/components/navbar/navbar.test.js
+++ stories/components/navbar/navbar.test.js
@@ -1,97 +1,97 @@
-describe('NAMEOFCOMPONENTGROUP', () => {
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+describe('Navbar', () => {
+  it('example visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--example');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('brand visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--brand');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('nav visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--nav');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('dropdown visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--dropdown');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('forms visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--forms');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('text visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--text');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('mix visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--mix');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('colors visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--colors');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('containers visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--containers');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('placement visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--placement');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
+  it('responsive visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    await page.goto('http://localhost:9009/iframe.html?id=components-navbar--responsive');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
