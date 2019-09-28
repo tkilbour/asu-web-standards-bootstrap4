b0911733fc5e4ee8b2d4deb79aaacbb7cf7d84a6
Bryan Barker <bryan.barker@asu.edu>
2019-09-28 16:34:17 -0700

    new example format for components

---
+++ stories/components/navbar/navbar.stories.js
@@ -0,0 +1,50 @@
+import { document, console } from 'global';
+import { storiesOf } from '@storybook/html';
+
+storiesOf('Components/NAMEOFCOMPONENTGROUP', module)
+  .addParameters({
+    happo: false,
+  })
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+.add('NAMEOFCOMPONENT', () => `
+COMPONENTCODE
+`)
+
+
+; //close
---
+++ stories/components/navbar/navbar.test.js
@@ -0,0 +1,101 @@
+describe('NAMEOFCOMPONENTGROUP', () => {
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('nAMEOFCOMPONENT visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?id=components-nAMEOFCOMPONENTGROUP--nAMEOFCOMPONENT');
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+
+}); //close
